4f8c9dedbede6c44235f19175419ecdedd6e9211
Kitka <elzord745@gmail.com>
2025-01-28 12:27:23 +0100

    First start

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello")
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from privateINF.ton import toke
+
+bot = Bot(token=toke)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await  message.reply("shdjflsjd")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
