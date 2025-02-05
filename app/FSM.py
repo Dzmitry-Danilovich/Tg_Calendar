9aa65f2a136a27d7ae28da6c865b539a9ad9b0f0
Kitka <danilovich.dzmitry@icloud.com>
2025-02-05 19:52:24 +0100

    Corect code and cheking

--- app/FSM.py
+++ app/FSM.py
@@ -3,7 +3,10 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.state import State, StatesGroup
 import app.keyboards as kb
+import sqlite3
+from datetime import datetime
 
+db = sqlite3.connect('telegram_event.db')
 router = Router()
 
 class Event(StatesGroup):
@@ -24,26 +27,30 @@ async def add_event(callback: CallbackQuery, state: FSMContext):
 
 @router.message(Event.event)
 async def add_event_two(message: Message, state: FSMContext):
-    await state.update_data(datas= message.text)
-    await state.set_state(Event.datas)
-    await message.answer("Podaj Zdarzenie: ")
+    try:
+        datetime.strptime(message.text, '%d-%m-%Y')
+        await state.update_data(datas= message.text)
+        await state.set_state(Event.datas)
+        await message.answer("Podaj Zdarzenie: ")
+    except ValueError:
+        await message.answer("Data jest nie poprawna")
 
 @router.message(Event.datas)
 async def add_data(message: Message, state: FSMContext):
     await state.update_data(event= message.text)
-    person_event = await state.get_data()
-    await message.answer("Akrualizowanie ukończone")
-    await message.answer(f"Data: {person_event}")
     await state.set_state(Event.time_ev)
-    await message.answer('O któtej chcesz przypomnienie o zdarzeniu?')
+    await message.answer('O któtej chcesz przypomnienie o zdarzeniu?\nNapisz w postaci: HH:MM')
 
 @router.message(Event.time_ev)
 async def add_time(message: Message, state: FSMContext):
-    await state.update_data(time_ev=message.text)
-    person_event = await state.get_data()
-    await message.answer(f"Data: {person_event['datas']} \n Event: {person_event['event']} \n Godzina przypomnienia {person_event['time_ev']}")
-    await message.answer('Czy chesz zapisać zdarzenie w Calendar?', reply_markup=kb.saves)
-
+    try:
+        datetime.strptime(message.text, '%H:%M')
+        await state.update_data(time_ev=message.text)
+        person_event = await state.get_data()
+        await message.answer(f"Data: {person_event['datas']} \n Event: {person_event['event']} \n Godzina przypomnienia {person_event['time_ev']}")
+        await message.answer('Czy chesz zapisać zdarzenie w Calendar?', reply_markup=kb.saves)
+    except ValueError:
+        await message.answer('Podana zła godzina')
 
 @router.callback_query(F.data ==  'tak')
 async def zapisz(message: Message, state: FSMContext):
@@ -52,5 +59,7 @@ async def zapisz(message: Message, state: FSMContext):
 
 @router.callback_query(F.data == 'nie')
 async def delete(message: Message, state: FSMContext):
+    await message.answer('jest tekst')
     await state.clear()
 
+db.close()
